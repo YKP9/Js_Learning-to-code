9eea83ae469e58f32a436500f1a23fa929c99d18
YKP9 <118888753+YKP9@users.noreply.github.com>
2023-09-10 10:11:40 +0000

    topic_1 done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 45687
+var accountEmail = "ykp.cpm" 
+let accountPassword = "4687ee"
+accountCity = "jalna"
+let accountState
+
+accountEmail="pky.com"
+accountState= "Maharashtra"
+
+console.table([accountId,accountEmail,accountPassword,accountState,accountCity])
+
+// do not use var or avoid it because it has issues in block scope and function scope
