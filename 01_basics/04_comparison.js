57ddb74b0ccec3c4ee1e2a1fcb0c59630bc181bc
YKP9 <118888753+YKP9@users.noreply.github.com>
2023-09-13 09:53:02 +0000

    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// // comparison operators
+
+// console.log(9>7)
+// console.log(9<7)
+// console.log(9>=7)
+// console.log(9<=7)
+
+
+// // EQUALITY CHECK OPERATORS
+// console.log(9==7)
+// console.log(9!=7)
+
+// //Operators with different datatypes
+// console.log("9" > 7)  // string is converted into number
+// console.log("9" > "7")  // string is converted into number
+
+
+// console.log(undefined > 0);
+// console.log(undefined >= 0);    
+// console.log(undefined < 0);
+// console.log(undefined <= 0);
+
+// strict check =>  === does not allow comparison with different datatypes 
+
+console.log("9"===7);
+
+
+
+
+
