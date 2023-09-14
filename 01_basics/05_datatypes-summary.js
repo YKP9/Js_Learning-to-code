012f5c9475c26708b90f2481f1c7d0ca9d242d2f
YKP9 <118888753+YKP9@users.noreply.github.com>
2023-09-14 08:29:17 +0000

    datatypes summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,63 @@
+// datatypes are of two types
+// Primitive DT and Non Primitive DT
+// PDT OR CALL VALUE : NNSSBBU
+// NUMBER NULL SYMBOL STRING BOOLEAN BIGINT UNDEFINED
+// PDT STORE THE VALUES AND THE CHANGES ARE MADE IN A COPY
+
+const score = 100  
+const PI = 3.14
+
+let target = null
+let str  = "Yashwanth"
+
+let id = Symbol("197")
+let anotherId = Symbol("457")
+
+let isLoggedIn = true
+let age
+
+let bigNum = 34560580232573238849443074744747474747474n
+
+// N-PDT OR REFERENCE TYPE
+// ARRAYS OBJECTS AND FUNCTIONS
+
+const names = ["Thor", "Shaktiman", "Jaadu", "Doraemon"]
+
+let personData =  {
+    name : "Yashwanth",
+    age : 22,
+    city : "Jalna"
+}
+
+let hello = function (){
+    console.log("Hello Duniya");
+}
+
+// console.log(hello);
+
+// console.table([typeof hello,typeof personData,typeof names]);
+
+// console.log(typeof score);
+// console.log(typeof PI);
+// console.log(typeof target);
+// console.log(typeof str);
+// console.log(typeof id);
+// console.log(typeof anotherId);
+// console.log(typeof isLoggedIn);
+// console.log(typeof age);
+// console.log(typeof bigNum);
+// console.log(typeof names);
+// console.log(typeof personData);
+// console.log(typeof hello);
+
+// DATATYPES TYPEOF
+// number = number
+// null = Object
+// string = string
+// Symbol = Symbol
+// boolean = boolean 
+// BigInt = BigInt
+// undefined = undefined
+// Arrays = object 
+// object = object 
+// function = function object
