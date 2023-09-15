4407502d71d8aff178e9f379c1cd79e9c795bba2
YKP9 <118888753+YKP9@users.noreply.github.com>
2023-09-15 11:43:24 +0000

    strings few methods done

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,24 @@
+let myName = "Yashwanth"
+let myAge = 21
+
+// console.log(myName + myAge); // one way to concatenate string
+
+// console.log(`My name is ${myName} & age is ${myAge}` ); // another way to concanetate string called as string interpolation . in ( ${expression} ) this is called placeholders where we can perform any expression and set it in the string.
+ 
+let favBook = new String('NeverFinished') // aanother way to declare string.
+// console.log(favBook.endsWith());
+
+// let myNum = 9994748497
+// let num = myNum.toString()
+// console.log(typeof num);
+
+// reverse a string
+
+// let revString = favBook.split('')
+// console.log(revString);
+// let newString = revString.reverse()
+// console.log(newString);
+// let reversedString= newString.join("")
+// console.log(`The reversed string is  ${reversedString}`);
+
+
