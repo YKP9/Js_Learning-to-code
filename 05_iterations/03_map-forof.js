483778866ce75299beba8cab51a026898fab7fe9
YKP9 <118888753+YKP9@users.noreply.github.com>
2023-10-05 10:55:47 +0000

    half done

---
+++ 05_iterations/03_map-forof.js
@@ -0,0 +1,53 @@
+// for of loop => used mainly on arrays 
+
+// for (const iterator of object) {
+    
+// }
+ // above iterator give any name 
+ // object perspective means a thing like it can be used on array strings and object
+
+ const arr = [1,86,87,15,7,44] 
+
+ for (const val of arr) {
+    console.log(val);
+    // console.log(val+10);
+ }
+
+ const gretting = "HELLO WORLD";
+
+ for (const greet of gretting) {
+    
+    if(greet===" "){
+        continue;
+    } 
+    // console.log(`message is ${greet}`);
+    
+ }
+
+ // space is skipped as continue is used
+
+ 
+ const randomQuote = "FINISH WHAT YOU START";
+
+ for (const quote of randomQuote) {
+    
+    if(quote==="U"){
+        break;
+    } 
+    // console.log(`message is ${quote}`);
+    
+ }
+
+ // after u iteration will be stopped due to break.
+ // we can also use break and continue in for of loop
+
+ const coutryCapitals = new Map()
+
+ coutryCapitals.set("India","Delhi")
+ coutryCapitals.set("France","Paris")
+ coutryCapitals.set("USA","New YORK")
+
+ for (const [key] of coutryCapitals) {
+    console.log(key);
+    
+ }
